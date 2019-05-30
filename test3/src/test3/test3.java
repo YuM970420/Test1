88ca3e12a103fb736c83f17e9ad2d553c532f0fb
YuM970420 <51185826+YuM970420@users.noreply.github.com>
2019-05-30 15:58:56 +0800

    Update test3.java

--- test3/src/test3/test3.java
+++ test3/src/test3/test3.java
@@ -4,12 +4,8 @@ public class test3 {
 <<<<<<< HEAD
 	public static void main(String[] args) {
 		System.out.println(111);
-		System.out.println(222);
-	}
-=======
-public static void main(String[] args) {
+		System.out.println(222)
 	System.out.println("aaaa");
 	System.out.println("bbbb");
 }
->>>>>>> refs/heads/branch-2
 }
